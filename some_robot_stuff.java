d2b58701f1b1d3efd22edd45d23b3c261e012a19
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-01-22 10:29:18 +0100

    Update some_robot_stuff.java

--- some_robot_stuff.java
+++ some_robot_stuff.java
@@ -1,26 +1,27 @@
 import java.awt.*;
-import java.awt.event.InputEvent;
+import java.awt.event.InputEvent; //implement the needed things
 
 public class Pffffff{
   
-  private Robot robot;
+  private Robot robot; //decalre robot
   public Pffffff(){
   try{
-    robot = new Robot();
+    robot = new Robot(); //tell what robot is , you have to sorround it with try catch because otherwise java has a problem
   }catch(Exception e){
     e.printStackTrace;
   }
     private void start(){
       
-      while(true){
-        int x = MouseInfo.getPointerInfo().getLocation.x
-        int y = MouseInfo.getPointerInfo().getLocation.y
-          Color hello = robot.getPixelColor(x, y);
-        System.out.print("X: " +x + " Y: " + y + "Color: " + hello+ "\r");
+      while(true){      // make sure the Location and color is live 
+        int x = MouseInfo.getPointerInfo().getLocation.x  //get the x Location of your Coursor with the help of  java.awt.event.InputEvent
+        int y = MouseInfo.getPointerInfo().getLocation.y  //get the y Location of your Coursor with the help of  java.awt.event.InputEvent
+          Color hello = robot.getPixelColor(x, y);        // get the color with the help of java.awt.robot
+        System.out.print("X: " +x + " Y: " + y + "Color: " + hello+ "\r"); //print that out
       }
     }
     public stativ void main (String[] args){
-      new Pffffff().start();
+      new Pffffff().start();    //execute start().
     }
 }
+  // Thank you that you allowed me to waste your time ;)
 
