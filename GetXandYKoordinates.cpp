ca50fa48c20575c2e42d07b469d207c4bf6f2054
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-01-27 18:43:34 +0100

    Update GetXandYKoordinates.cpp

--- GetXandYKoordinates.cpp
+++ GetXandYKoordinates.cpp
@@ -1,2 +1,20 @@
 //This file is written in Visual c++ using Microsoft Visual Studio 2019 Community
 #include <iostream>
+#include <Windows.h>
+using namespace std;
+int a = 1;
+int main(){
+  POINTER p = (NULL);
+  while(a == 1){
+    if(GetAsyncKeyState(VK_BACK)){
+      GetCursorPos(&p);
+      cout <<"x position: " <<p.x <<" y-position: " <<p.y <<endl;
+      Sleep(200);
+    }
+     if(GetAsyncKeyState(VK_SPACE)){
+      a = 2;
+      
+    }
+  }
+    
+}
