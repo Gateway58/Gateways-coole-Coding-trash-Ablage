134c9570a365bbd85cdd861cc5cf1285be3280bf
Gateway58 <73712423+Gateway58@users.noreply.github.com>
2021-02-04 10:38:58 +0100

    Update Teams.java

--- Teams.java
+++ Teams.java
@@ -7,19 +7,19 @@ public class Teams {
     private  Robot robot;
     public Teams(){
         try{
-            robot = new Robot();
+            robot = new Robot();        //creates Robot and stuff
         }catch (Exception e){
             e.printStackTrace();
         }
     }
      public  void start() {
-        int a = 3;
+        int a = 1;
 
          while (a == 1) {
              robot.mouseMove(1710, 1009);
 
              Color color = new Color(98, 100, 167);
-             Color colo = robot.getPixelColor(1735, 930);
+             Color colo = robot.getPixelColor(1735, 930);       //I could remove everything from the colors except color and colo, but im too lazy so sorry
              Color Mute = robot.getPixelColor(1075, 459);
              Color purple = new Color(139, 140, 199);
              Color purplefailsafe = new Color(166, 167, 220);
@@ -30,7 +30,7 @@ public class Teams {
                  robot.delay(10);
                  robot.mousePress(InputEvent.BUTTON1_MASK);
                  robot.delay(10);
-                 robot.mouseRelease(InputEvent.BUTTON1_MASK);
+                 robot.mouseRelease(InputEvent.BUTTON1_MASK);      //if this is true we move on to the interface that shows up when you will join a meeting
                  a = 2;
              }}
 
@@ -39,12 +39,12 @@ public class Teams {
 
                  Color color = new Color(98, 100, 167);
                  Color colo = robot.getPixelColor(1735, 930);
-                 Color Mute = robot.getPixelColor(1076, 462);
-                 Color purple = new Color(139, 140, 199);
+                 Color Mute = robot.getPixelColor(1076, 462);       //It just looks at the pixel and decides than what to do. The Pixels x and y coordinate are persolized for my VM-sceen (I use VirtualBox)
+                 Color purple = new Color(139, 140, 199);           //, you may have to change the  x and y coordinates but i think the color is the same.
                  Color purplefailsafe = new Color(166, 167, 220);
                  Color dark = new Color(32, 31, 31);
                  try{
-                     Thread.sleep(10000);
+                     Thread.sleep(10000);                           //It sleeps so there are not so many unwanted results 
                  } catch (InterruptedException e) {
                      e.printStackTrace();
                  }
@@ -86,7 +86,7 @@ public class Teams {
                      robot.mousePress(InputEvent.BUTTON1_MASK);
                      robot.delay(10);
                      robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                    a = 3;
+                    a = 3;                                              //Now we joined the meeting with the mic muted now it checks IF the mic is muted
 
                  }
 
@@ -107,9 +107,9 @@ public class Teams {
                 Color unmuted = new Color(255, 255, 255);
                 Color Mutes = robot.getPixelColor(1760, 1000);
                 Color unmutes = new Color(131, 130, 130);
-                Color Mutefailsafe = robot.getPixelColor(1746, 993);
+                Color Mutefailsafe = robot.getPixelColor(1746, 993);        //again these are my x and y coordinates you may have to change them
                 if(muterule.equals(unmuted)){
-                    robot.delay(3542);
+                    robot.delay(3542);                                      //If we are unmuted, it automatically mutes us in a couple of seconds
                     robot.mouseMove(1680, 70);
                     robot.mousePress(InputEvent.BUTTON1_MASK);
                     robot.delay(1);
@@ -156,6 +156,6 @@ public class Teams {
 
     public static void main(String[] args) {
         Teams teams = new Teams();
-        teams.start();
+        teams.start();              //it execudes the code above
     }
 }
